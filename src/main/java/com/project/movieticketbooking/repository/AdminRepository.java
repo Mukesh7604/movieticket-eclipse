33c74184f3a582fe84189cc705b26e13edb89bf9
Janarthanan-16 <janarthanan.m1609@gmail.com>
2024-12-26 15:54:27 +0530

    First Commit

---
+++ src/main/java/com/project/movieticketbooking/repository/AdminRepository.java
@@ -0,0 +1,8 @@
+package com.project.movieticketbooking.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.project.movieticketbooking.entity.Admin;
+
+public interface AdminRepository extends JpaRepository<Admin, Integer> {
+
+}
